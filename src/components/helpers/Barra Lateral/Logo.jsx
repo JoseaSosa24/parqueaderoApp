ea598c5583d0d41e771391d5f4947c86ffc6fb32
manunovoa <manunovoa0415@gmail.com>
2022-12-04 11:38:11 -0500

    Cree rama manuela, modificando barra lateral

--- src/components/helpers/Barra Lateral/Logo.jsx
+++ src/components/helpers/Barra Lateral/Logo.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Logo = () => {
     return (
-        <section className='item-encabezado d-flex flex-column align-items-center'>
-            <img src='../../../../src/assets/img/logo.png' width='130px'/>
-            <h4>PARQUEADERO EL CHAMO</h4>
+        <section className='item-encabezado d-flex flex-column align-items-center gap-5'>
+            <img src='../../../../src/assets/img/logo.png' width='120px'/>
+            <h4 className='text-white'>PARQUEADERO EL CHAMO</h4>
         </section>
     )
 }
