3fc2544c8b271bf48b84c13f1203b6e978bdf92d
manunovoa <manunovoa0415@gmail.com>
2022-12-06 19:28:01 -0500

    se crea la estructura de los demas formularios

--- src/components/helpers/Formularios/Clientes/Clientes.jsx
+++ src/components/helpers/Formularios/Clientes/Clientes.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Titulo } from "../Titulo";
 import { FormInput } from "../FormInput";
 import { Buscar } from "../Buscar";
-import { Table } from "../table/Table";
+import { Table } from "../Table/Table";
 
-export const RegistroCliente = () => {
+export const Clientes = () => {
   return (
     <section className="registro-cliente m-4">
       <Titulo textTitulo={"Registro Cliente:"} />
       <section className="formulario ">
-        <FormInput classInput={"item-form"} infomacionInput={"Documento: "} />
-        <FormInput classInput={"item-form"} infomacionInput={"Nombre: "} />
-        <FormInput classInput={"item-form"} infomacionInput={"Correo: "} />
-        <FormInput classInput={"item-form"} infomacionInput={"Direccion: "} />
-        <FormInput classInput={"item-form"} infomacionInput={"Celular: "} />
+        <FormInput classInput={"item-form"} tipoInput={"text"} infomacionInput={"Documento: "} />
+        <FormInput classInput={"item-form"} tipoInput={"text"} infomacionInput={"Nombre: "} />
+        <FormInput classInput={"item-form"} tipoInput={"text"} infomacionInput={"Correo: "} />
+        <FormInput classInput={"item-form"} tipoInput={"text"} infomacionInput={"Direccion: "} />
+        <FormInput classInput={"item-form"} tipoInput={"text"} infomacionInput={"Celular: "} />
         <input className="guardar" type="button" value={"Guardar"} />
       </section>
       <section className="seccion-buscar d-flex mt-4 ">
--- src/components/helpers/Formularios/FormInput.jsx
+++ src/components/helpers/Formularios/FormInput.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const FormInput = ({ infomacionInput, classInput }) => {
+export const FormInput = ({ infomacionInput, classInput, tipoInput }) => {
   return (
     <section className="inputs">
       <h3 className="text-white fs-5">{infomacionInput}</h3>
-      <input className={classInput} type="text" />
+      <input className={classInput} type={tipoInput} />
     </section>
   );
 };
