1543fef62ec39ad3760ddfbd3d14b84256830c9e
JOSE SOSA <josea_1998@hotmail.com>
2022-12-07 01:35:08 -0500

    Se importa e implenta el BrowserRouter en el mains. Se importa e implenta el componente Link del router dom e los items de la barra lateral, debido a que es una componente contenedorSE DEBEN MODIFICAR ESTILOS PARA Oganizarlo.

--- src/components/helpers/Barra Lateral/MenuItem.jsx
+++ src/components/helpers/Barra Lateral/MenuItem.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link, Outlet } from 'react-router-dom'
 
-export const MenuItem = ({ src, itemText, classImg }) => {
+export const MenuItem = ({ src, itemText, classImg,to,href }) => {
   return (
+
     <section className="menu-item d-flex align-items-center justify-content-center">
-      <img src={src} className={classImg} id="" width="38" height="" />
-      <a id="">{itemText}</a>
+      <Link to={to} className='menu-item-link text-decoration-none d-flex align-items-center justify-content-center' hrefLang={href}>
+        <img src={src} className={classImg} id="" width="38" height="" />
+        <a id="">{itemText}</a>
+      </Link>
+      <Outlet />
     </section>
+
   );
 };
