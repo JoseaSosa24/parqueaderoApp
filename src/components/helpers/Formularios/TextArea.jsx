83d38346b24c77adb8d17af9753b79e0ad09d170
JOSE SOSA <josea_1998@hotmail.com>
2022-12-08 18:09:09 -0500

    Se realizan modificaciones a la disposicion del main, se cambian tamaños a los formularios.

--- src/components/helpers/Formularios/TextArea.jsx
+++ src/components/helpers/Formularios/TextArea.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const TextArea = ({nameTextArea, idTextArea}) => {
+export const TextArea = ({ nameTextArea, idTextArea,classSection }) => {
   return (
-    <section className="inputs">
+    <section className={"inputs "+classSection}>
       <h3 className="text-white fs-5">Descripcion:</h3>
-    <textarea id={idTextArea} name={nameTextArea} rows="5" cols="33"></textarea>
+      <textarea id={idTextArea} name={nameTextArea} rows="3" ></textarea>
     </section>
   );
 };
