a244194486f3db2ea039041782cbac37e17d8e68
JOSE SOSA <josea_1998@hotmail.com>
2022-12-07 21:33:43 -0500

    se modifica un estilo de un menu-item

--- src/components/helpers/Barra Lateral/MenuItem.jsx
+++ src/components/helpers/Barra Lateral/MenuItem.jsx
@@ -5,13 +5,11 @@ export const MenuItem = ({ src, itemText, classImg, to, href }) => {
   return (
 
     <section className="menu-item d-flex align-items-center justify-content-center">
-
       <Link to={to} className='menu-item-link text-decoration-none d-flex align-items-center justify-content-center' hrefLang={href}>
         <img src={src} className={classImg} id="" width="38" height="" />
         {itemText}
       </Link>
       <Outlet />
-
     </section>
 
   );
