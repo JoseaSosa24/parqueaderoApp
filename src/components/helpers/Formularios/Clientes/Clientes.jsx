d3886204ad1cc8c6a8436c5b5f26289a3c2a7a7c
manunovoa <manunovoa0415@gmail.com>
2022-12-08 16:27:38 -0500

    Se organizo los demas formularios con columnas de bootstrap

--- src/components/helpers/Formularios/Clientes/Clientes.jsx
+++ src/components/helpers/Formularios/Clientes/Clientes.jsx
@@ -11,14 +11,13 @@ export const Clientes = () => {
     <section className="registro-cliente m-4">
       <Titulo textTitulo={"Registro Cliente:"} />
       <section className="formulario d-flex align-items-center justify-content-center p-4">
-        <form className="formulario-clientes row col-12 d-flex g-3 formulario-clientes">
+        <form className="formulario-clientes row col-12 d-flex g-3 ">
           <FormInput classSection={'col-3'} classInput={"item-form"} tipoInput={"text"} infomacionInput={"Documento: "} inputId={'documento'} inputName={'documento'} inputPlaceholder={'10364845'} />
           <FormInput classSection={'col-4'} classInput={"item-form"} tipoInput={"text"} infomacionInput={"Nombre: "} inputId={'nombre'} inputName={'nombre'} inputPlaceholder={'Juan Perez'} />
           <FormInput classSection={'col-5'} classInput={"item-form"} tipoInput={"text"} infomacionInput={"Correo: "} inputId={'correo'} inputName={'correo'} inputPlaceholder={'juanperez@gmail.com'} />
           <FormInput classSection={'col-5'} classInput={"item-form"} tipoInput={"text"} infomacionInput={"Direccion: "} inputId={'direccion'} inputName={'direccion'} inputPlaceholder={'CRRA 2 CL SUR B'} />
           <FormInput classSection={'col-3'} classInput={"item-form"} tipoInput={"text"} infomacionInput={"Celular: "} inputId={'celular'} inputName={'celular'} inputPlaceholder={'3225556898'} />
-          <Button  clase={'form-button d-flex justify-content-center col-12'} classButton={'guardar form-button col-3'} textButton={'Enviar'} type={'submit'}/>
-          
+          <Button  clase={'form-button d-flex justify-content-center col-12'} classButton={'guardar form-button col-3'} textButton={'Guardar'} type={'submit'}/>
         </form>
       </section>
 
