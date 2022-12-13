c713233e00a34829b268825d42d3ff624037fdaf
JOSE SOSA <josea_1998@hotmail.com>
2022-12-12 23:12:59 -0500

    Se instala libreria Formik para validacion de formularios. Se modifica el formulario de cliente utilizando la estructura de formik. Se borra elementos de el componete FormInput

--- src/components/helpers/Formularios/Clientes/Clientes.jsx
+++ src/components/helpers/Formularios/Clientes/Clientes.jsx
@@ -7,16 +7,18 @@ import { Button } from "../Button";
 import axios from "axios";
 import { Mensaje } from "../Mensaje";
 import swal from 'sweetalert';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
 
 const URI = 'http://localhost:3100/clientes'
 export const Clientes = () => {
-  const [documento, setDocumento] = useState({ campo: '', valido: null });
-  const [nombre, setNombre] = useState({ campo: '', valido: null });
-  const [correo, setCorreo] = useState({ campo: '', valido: null });
-  const [direccion, setDireccion] = useState({ campo: '', valido: null });
-  const [celular, setCelular] = useState({ campo: '', valido: null });
-  const [formularioValido, setFormularioValido] = useState(null);
-
+  /*   const [documento, setDocumento] = useState({ campo: '', valido: null });
+    const [nombre, setNombre] = useState({ campo: '', valido: null });
+    const [correo, setCorreo] = useState({ campo: '', valido: null });
+    const [direccion, setDireccion] = useState({ campo: '', valido: null });
+    const [celular, setCelular] = useState({ campo: '', valido: null });
+    const [formularioValido, setFormularioValido] = useState(null);
+    const [formularioEnviado, cambiarFormularioEnviado] = useState(false); */
   const expresionRegular = {
     usuario: /^[a-zA-Z0-9\_]{4,16}$/, // Letras, numeros, guion_bajo
     nombre: /^[a-zA-ZÀ-ÿ\s]{3,20}$/, // Letras y espacios, pueden llevar acentos.
@@ -27,17 +29,17 @@ export const Clientes = () => {
   };
 
   const correcto = (e) => {
-    swal({
-      title: "Mensaje de éxito",
-      text: "¡Cliente agregado correctamente!",
-      icon: "success",
-      buttons: "ok"
-    })
+     swal({
+       title: "Mensaje de éxito",
+       text: "¡Cliente agregado correctamente!",
+       icon: "success",
+       buttons: "ok"
+     }) 
+ 
+ 
+   }
 
-
-  }
-
-  const createCliente = async (e) => {
+  /* const createCliente = async (e) => {
     //e.preventDefault()//Buscar la forma que guarde y muestre el registro sin recargar la pagina
     e.preventDefault()
     if (
@@ -66,132 +68,163 @@ export const Clientes = () => {
       setFormularioValido(false)
 
     }
-  }
+  } */
 
-/*
-  const buscarPorId = async (e)=>{
-    //e.preventDefault();
-    let dato = e.target.value;
-    console.log(dato);
-    const res = await axios.get(`${URI}/${dato}`)
-    setDocumento({campo: res.cedCliente, valido: null});
-    setCorreo({campo:res.correo, valido: null});
-    setDireccion({campo: res.direccion, valido: null})
-    setCelular({campo: res.celular, valido: null})
-    setNombre({campo: res.nombre, valido: null})
-
-  }
-
-  useEffect(()=>{
-    buscarPorId();
-  }, [])
-*/
+  /*
+    const buscarPorId = async (e)=>{
+      //e.preventDefault();
+      let dato = e.target.value;
+      console.log(dato);
+      const res = await axios.get(`${URI}/${dato}`)
+      setDocumento({campo: res.cedCliente, valido: null});
+      setCorreo({campo:res.correo, valido: null});
+      setDireccion({campo: res.direccion, valido: null})
+      setCelular({campo: res.celular, valido: null})
+      setNombre({campo: res.nombre, valido: null})
+  
+    }
+  
+    useEffect(()=>{
+      buscarPorId();
+    }, [])
+  */
 
   return (
-    <section className="registro-cliente m-4">
-      <Titulo textTitulo={"Registro Cliente:"} />
-      <section className="formulario d-flex align-items-center justify-content-center p-4">
-        <form action="" className="formulario-clientes row col-12 d-flex g-3 " onSubmit={createCliente}>
+    <>
 
-          <FormInput classSection={'col-3'}
-            estado={documento}
-            cambiarEstado={setDocumento}
-            classInput={"item-form"}
-            tipoInput={"text"}
-            infomacionInput={"Documento: "}
-            inputId={'documento'} inputName={'documento'}
-            inputPlaceholder={'10364845'}
-            /*onChange={(e) => { setDocumento(e.target.value) }}*/
-            /*onkeypress={"if (e.keyCode < 45 || event.keyCode > 57) event.returnValue = false;"}*/
-            onkeypress={(e) => { if (e.keyCode < 48 || e.keyCode > 57) e.returnValue = false }}
-            maxlength="10"
-            pattern={"[0-9]{9,10}"}
-            title='Sólo números. Minímo 9, Máximo 10 digitos'
-            expresionRegular={expresionRegular.documento}
-          />
+      <section className="registro-cliente m-4">
+        <Titulo textTitulo={"Registro Cliente:"} />
+        <section className="formulario d-flex align-items-center justify-content-center p-4">
 
-          <FormInput classSection={'col-4'}
-            estado={nombre}
-            cambiarEstado={setNombre}
-            classInput={"item-form"}
-            tipoInput={"text"}
-            infomacionInput={"Nombre: "}
-            inputId={'nombre'}
-            inputName={'nombre'}
-            inputPlaceholder={'Juan Perez'}
-            /*onChange={(e) => { setNombre(e.target.value) }}*/
-            title='Minímo 3 letras,máximo 35'
-            pattern={"[A-Za-zÀ-ÿ ]{2,35}"}
-            maxlength={35}
-            expresionRegular={expresionRegular.nombre}
-          />
+          <Formik
+            initialValues={{
+              documento: '',
+              nombre: '',
+              correo: '',
+              direccion: '',
+              celular: ''
+            }}
 
-          <FormInput classSection={'col-5'}
-            estado={correo}
-            cambiarEstado={setCorreo}
-            classInput={"item-form"}
-            tipoInput={"email"}
-            infomacionInput={"Correo: "}
-            inputId={'correo'}
-            inputName={'correo'}
-            inputPlaceholder={'juanperez@gmail.com'}
-            expresionRegular={expresionRegular.correo}
-          /*onChange={(e) => { setCorreo(e.target.value) }}*/
-          />
+            validate={(valores) => {
+              let errores = {};
 
-          <FormInput classSection={'col-5'}
-            estado={direccion}
-            cambiarEstado={setDireccion}
-            classInput={"item-form"}
-            tipoInput={"text"}
-            infomacionInput={"Direccion: "}
-            inputId={'direccion'}
-            inputName={'direccion'}
-            inputPlaceholder={'CRRA 2 CL SUR B'}
-            title='La dirección minima de 5 letras'
-            pattern={"[A-Za-z0-9À-ÿ ,-_#]{7,50}"}
-            expresionRegular={/^[A-Za-z0-9À-ÿ ,-_]{7,50}$/}
-          /*onChange={(e) => { setDireccion(e.target.value) }}*/
-          />
-
-          <FormInput classSection={'col-4'}
-            estado={celular}
-            cambiarEstado={setCelular}
-            classInput={"item-form"}
-            tipoInput={"number"}
-            infomacionInput={"Celular: "}
-            inputId={'celular'}
-            inputName={'celular'}
-            inputPlaceholder={'3225556898'}
-            /*onChange={(e) => { setCelular(e.target.value) }}*/
-            expresionRegular={expresionRegular.celular}
-            pattern={"[0-9]{10}"}
-            title='El celular debe ser de 10 digitos'
-            maxlength="10" />
-
-          {formularioValido === false &&
-            <Mensaje classMensaje={'alert alert-danger'}
-              mensaje={'Campos incorrectos o faltan por rellenar.'} />
-          }
-          <Button clase={'form-button d-flex justify-content-center col-12'}
-            classButton={'guardar form-button col-3'}
-            textButton={'Guardar'} type={'submit'} />
-        </form>
-      </section>
-
-      
-      <TableClientes
-        textoColumna1={"Documento"}
-        textoColumna2={"Nombre"}
-        textoColumna3={"Correo"}
-        textoColumna4={"Direccion"}
-        textoColumna5={"Celular"}
-        textoColumna6={"Accion"}
-
-      />
+              if (!valores.documento) {
+                errores.documento = 'Por favor ingresa un documento'
+              } else if (!expresionRegular.documento.test(valores.documento)) {
+                errores.documento = 'El documento debe minimo 9 máximo 10 digitos'
+              }
+              
+              if (!valores.nombre) {
+                errores.nombre = 'Por favor ingresa un nombre'
+              } else if (!expresionRegular.nombre.test(valores.nombre)) {
+                errores.nombre = 'El nombre solo puede contener letras y espacios'
+              }
+              
+              if (!valores.correo) {
+                errores.correo = 'Por favor ingresa un correo'
+              } else if (!expresionRegular.correo.test(valores.correo)) {
+                errores.correo = 'El correo solo puede contener letras y espacios'
+              }
+              
+              if (!valores.direccion) {
+                errores.direccion = 'Por favor ingresa un direccion'
+              } /*else if (!expresionRegular.direccion.test(valores.direccion)) {
+                errores.direccion = 'El direccion debe contener @ ,.'
+              }*/
+              if (!valores.celular) {
+                errores.celular = 'Por favor ingresa un celular'
+              } else if (!expresionRegular.celular.test(valores.celular)) {
+                errores.celular = 'El celular solo puede contener numeros'
+              }
 
 
-      {/* <Table
+              return errores;
+            }}
+          >
+            {({ errors }) => (
+              <section className="formulario d-flex align-items-center justify-content-center p-4 w-100">
+                <Form className="formulario-clientes row col-12 d-flex g-3 ">
+                  <section className="col-3">
+                    <h3 className="text-white fs-5" >Documento:</h3>
+                    <Field
+                      className="form-control item-form"
+                      type="text"
+                      id="documento"
+                      name="documento"
+                      placeholder="10364845"
+                    />
+                    <ErrorMessage name="documento" component={() => (<section className="error text-danger">{errors.documento}</section>)} />
+                  </section>
+                  <section className="col-4">
+                    <h3 className="text-white fs-5" >Nombre:</h3>
+                    <Field
+                      className="form-control item-form"
+                      type="text"
+                      id="nombre"
+                      name="nombre"
+                      placeholder="Juan Perez"
+                    />
+                    <ErrorMessage name="nombre" component={() => (<section className="error text-danger">{errors.nombre}</section>)} />
+                  </section>
+                  <section className="col-5">
+                    <h3 className="text-white fs-5" >Correo:</h3>
+                    <Field
+                      className="form-control item-form"
+                      type="email"
+                      id="correo"
+                      name="correo"
+                      placeholder="juanperez@gmail.com"
+                    />
+                    <ErrorMessage name="correo" component={() => (<section className="error text-danger">{errors.correo}</section>)} />
+                  </section>
+                  <section className="col-5">
+                    <h3 className="text-white fs-5" >Dirección:</h3>
+                    <Field
+                      className="form-control item-form"
+                      type="text"
+                      id="direccion"
+                      name="direccion"
+                      placeholder="CRRA 2 CL SUR B"
+                    />
+                    <ErrorMessage name="direccion" component={() => (<section className="error text-danger">{errors.direccion}</section>)} />
+                  </section>
+                  <section className="col-4">
+                    <h3 className="text-white fs-5" >Celular: </h3>
+                    <Field
+                      className="form-control item-form"
+                      type="text"
+                      id="celular"
+                      name="celular"
+                      placeholder="3225556898"
+                    />
+                    <ErrorMessage name="celular" component={() => (<p className="error text-danger">{errors.celular}</p>)} />
+                  </section>
+                  <Button clase={'form-button d-flex justify-content-center col-12'}
+                    classButton={'guardar form-button col-3'}
+                    textButton={'Guardar'} type={'submit'} />
+                </Form>
+              </section>
+            )}
+
+
+          </Formik>
+
+
+        </section>
+
+
+        <TableClientes
+          textoColumna1={"Documento"}
+          textoColumna2={"Nombre"}
+          textoColumna3={"Correo"}
+          textoColumna4={"Direccion"}
+          textoColumna5={"Celular"}
+          textoColumna6={"Accion"}
+
+        />
+
+
+        {/* <Table
         textoColumna1={"Documento"}
         textoColumna2={"Nombre"}
         textoColumna3={"Correo"}
@@ -199,6 +232,9 @@ export const Clientes = () => {
         textoColumna5={"Celular"}
         textoColumna6={"Accion"}
       /> */}
-    </section>
+      </section>
+    </>
+
   );
-};
+
+}
--- src/components/helpers/Formularios/FormInput.jsx
+++ src/components/helpers/Formularios/FormInput.jsx
@@ -1,65 +1,22 @@
 import React from "react";
 
-export const FormInput = ({ estado, cambiarEstado, infomacionInput, classInput, tipoInput, inputName,
-  inputId, inputPlaceholder, classSection, expresionRegular, /* onChange */ required, title,
-  maxlength, onkeypress, pattern }) => {
-    let colorEstado = ' ';
+export const FormInput = ({infomacionInput, classInput, tipoInput, inputName,
+  inputId, inputPlaceholder, classSection, expresionRegular, title,
+  maxlength }) => {
+
     
-  const onChange = (e) => {
-    cambiarEstado({ ...estado, campo: e.target.value });
-  }
-
-  function cambiarColor() {
-    
-    if (estado.valido === 'true') {
-      colorEstado = ' bg-primary '
-    } else {
-      colorEstado = ' bg-primary '
-    }
-    return colorEstado
-  }
-
-  const validacion = () => {
-    if (expresionRegular) {
-      if (expresionRegular.test(estado.campo)) {
-        cambiarEstado({ ...estado, valido: 'true' });
-        
-        /* infomacionInput=infomacionInput+" correcto "+estado.campo */
-        console.log("CAMPO CORRECTO " + estado.valido)
-
-      } else {
-        cambiarEstado({ ...estado, valido: 'false' });
-        /* infomacionInput=infomacionInput+" incorrecto "+estado.campo */
-        console.log("CAMPO INCORRECTO " + estado.valido)
-      }
-
-    }
-
-    /* if(funcion){
-      funcion();
-    } */
-  }
-
-
+ 
   return (
     <section className={" " + classSection}>
       <h3 className="text-white fs-5">{infomacionInput}</h3>
       {console.log(estado)}
-      <input className={"form-control " + classInput+colorEstado}
+      <input className={"form-control " + classInput}
         id={inputId}
         type={tipoInput}
         name={inputName}
         placeholder={inputPlaceholder}
-        onChange={onChange}
-        value={estado}
-        /*onKeyUp={validacion}*/
-        /*onBlur={validacion}*/
         title={title}
         maxLength={maxlength}
-        /*onKeyDown={validacion}*/
-        pattern={pattern}
-        /*onKeyUp={onkeypress}*/
-        required={required}
       />
     </section>
   );
